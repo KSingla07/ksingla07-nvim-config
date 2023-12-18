87623c58e0ad6a9da01a4d193fdf4dce933775a4
Karan Singla <singlak19@gmail.com>
2023-12-19 01:51:03 +0530

    Initial setup using NvChad

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,25 @@
+local options = {
+  ensure_installed = {
+    "lua",
+    "go",
+    "gomod",
+    "gosum",
+    "gowork",
+    "dockerfile",
+    "yaml",
+    "make",
+    "sql",
+    "xml",
+    "bash",
+    "csv",
+  },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
